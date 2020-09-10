1c476d65d09942eb2134833cc8a1d0ec649ec832
Skye McCaskey <skye.mccaskey@hcahealthcare.com>
2020-09-10 17:19:43 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "TPObfuscation",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "TPObfuscation",
