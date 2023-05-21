b6501ca49fed16d67aa26a937ea1acbfaf649646
Philip Niedertscheider <me@philprime.dev>
2023-05-21 11:57:36 +0200

    chore(deps): updated to Ruby v3

--- vendor/bundle /ruby/2.7.0/gems/slather-2.7.1/spec/fixtures/FixtureFrameworkTests/FlashExperimentTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-import FixtureFramework
-
-class FlashExperimentTests: XCTestCase {
-    func testExample() throws {
-        let sut = FlashExperiment()
-        XCTAssertTrue(sut.isAwesome, "Your flash experiment isn't awesome!")
-    }
-}
