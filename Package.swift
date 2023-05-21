bcc46e08f7eab6dd66f02178797888f6b366b5eb
Philip Niedertscheider <me@philprime.dev>
2023-05-21 12:16:27 +0200

    test: removed test dependencies from deployment

--- Package.swift
+++ Package.swift
@@ -8,15 +8,15 @@ let package = Package(
         .library(name: "TPObfuscation", targets: ["TPObfuscation"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Quick", exact: "7.0.0"),
-        .package(url: "https://github.com/Quick/Nimble", exact: "12.0.0"),
+        //dev .package(url: "https://github.com/Quick/Quick", exact: "7.0.0"),
+        //dev .package(url: "https://github.com/Quick/Nimble", exact: "12.0.0"),
     ],
     targets: [
         .target(name: "TPObfuscation", path: "Source/Classes"),
-        .testTarget(name: "TPObfuscationTests", dependencies: [
-            "TPObfuscation",
-            "Quick",
-            "Nimble"
-        ])
+        //dev .testTarget(name: "TPObfuscationTests", dependencies: [
+        //dev     "TPObfuscation",
+        //dev     "Quick",
+        //dev     "Nimble"
+        //dev ])
     ]
 )
